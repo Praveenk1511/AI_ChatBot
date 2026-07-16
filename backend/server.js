13051e348c68abbedbc8aa06d1acd775e10b9eb3
praveen.k <praveen.k@mapmyindia.com>
2026-07-16 16:25:11 +0530

    add prodution url in corse

--- backend/server.js
+++ backend/server.js
@@ -13,6 +13,7 @@ const app = express();
 // Middleware
 app.use(cors({
   origin: "http://localhost:5173",
+  origin: "https://ai-chat-bot-chi-taupe.vercel.app",
   credentials: true,
 }));
 app.use(express.json());
